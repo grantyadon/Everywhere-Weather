a6692c71da811df6c6ca572c3e3b984f5315d8a3
grantyadon <55468005+grantyadon@users.noreply.github.com>
2024-11-11 19:34:03 -0600

    Updated SDK to 35 and fixed non-existent string

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.cs407.everywhereweather"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.cs407.everywhereweather"
